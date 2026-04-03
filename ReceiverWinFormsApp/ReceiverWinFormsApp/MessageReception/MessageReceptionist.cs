359016874341bcbb50917ce885e0834546e1c7f7
janos-laszlo <janos-laszlo@outlook.com>
2026-04-04 02:19:10 +0300

    Simplify server Socket

--- ReceiverWinFormsApp/ReceiverWinFormsApp/MessageReception/MessageReceptionist.cs
+++ ReceiverWinFormsApp/ReceiverWinFormsApp/MessageReception/MessageReceptionist.cs
@@ -2,101 +2,52 @@
 using Domain.MessageReception;
 using Microsoft.Extensions.Logging;
 using System;
+using System.Buffers;
 using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace ReceiverWinFormsApp.MessageReception
 {
     public class MessageReceptionist : IMessageReceptionist
     {
-        readonly ManualResetEvent allDone = new(false);
-        private readonly ILogger<MessageReceptionist> logger;
         const char MessageTerminator = '\n';
         const int Port = 11000;
-        IPEndPoint localEndPoint;
-        Socket listener;
+        readonly ILogger<MessageReceptionist> logger;
+        readonly IPEndPoint localEndpoint;
+
         bool shouldRun = false;
-        Thread messageListenerThread;
-        private UdpClient udpClient;
+        TcpListener messageListener;
+        UdpClient udpClient;
 
         public MessageReceptionist(ILogger<MessageReceptionist> logger)
         {
             this.logger = logger;
 
-            SetLocalEndPoint();
-            SubscribeToNetworkAddressChangedEvent();
+            localEndpoint = GetLocalEndPoint();
+            NetworkChange.NetworkAddressChanged += AddressChangedCallback;
         }
 
         public void Start()
         {
             shouldRun = true;
-            StartMessageListener();
+            Task.Run(StartMessageReceptionist);
             StartNameLookupReceptionist();
-        }
-
-        private void StartMessageListener()
-        {
-            messageListenerThread = new Thread(() =>
-            {
-                // Create a TCP/IP socket.  
-                listener = new Socket(
-                    addressFamily: localEndPoint.AddressFamily,
-                    SocketType.Stream,
-                    ProtocolType.Tcp);
-                // Bind the socket to the local endpoint and listen for incoming connections.  
-                try
-                {
-                    listener.Bind(localEndPoint);
-                    listener.Listen(10);
-                    logger.LogInformation("Listening for a connection...");
-                    while (shouldRun)
-                    {
-                        // Set the event to nonsignaled state.  
-                        allDone.Reset();
-                        // Start an asynchronous socket to listen for connections.                          
-                        listener.BeginAccept(
-                            new AsyncCallback(AcceptCallback),
-                            listener);
-                        // Wait until a connection is made before continuing.
-                        allDone.WaitOne();
-                    }
-                }
-                catch (Exception ex)
-                {
-                    logger.LogInformation(ex, "Excpetion while listening for messages");
-                }
-                finally
-                {
-                    listener.Dispose();
-                }
-            });
-            messageListenerThread.Start();
         }
 
         public void Stop()
         {
             shouldRun = false;
-            StopMessageListener();
-            StopNameLookupReceptionist();
+            messageListener.Stop();
+            udpClient.Close();
         }
 
         public Func<string, Maybe<string>> MessageProcessor { get; set; } = x => Maybe<string>.None();
 
-        private void StopMessageListener()
-        {
-            allDone.Set();
-            WaitForMessageListenerThread();
-        }
-
-        private void StopNameLookupReceptionist()
-        {
-            udpClient.Close();
-        }
-
-        private void SetLocalEndPoint()
+        private IPEndPoint GetLocalEndPoint()
         {
             IPAddress ip = GetMyIP();
             while (!IPAddressValid(ip))
@@ -107,7 +58,7 @@ namespace ReceiverWinFormsApp.MessageReception
             }
 
             logger.LogInformation("My IP:{IP}", ip);
-            localEndPoint = new IPEndPoint(ip, Port);
+            return new IPEndPoint(ip, Port);
         }
 
         private static IPAddress GetMyIP()
@@ -123,25 +74,18 @@ namespace ReceiverWinFormsApp.MessageReception
             return IPAddress.Loopback;
         }
 
-        private static bool IPAddressValid(IPAddress ip)
-        {
-            return !IPAddress.Loopback.Equals(ip);
-        }
-
-        private void SubscribeToNetworkAddressChangedEvent()
-        {
-            NetworkChange.NetworkAddressChanged += AddressChangedCallback;
-        }
+        private static bool IPAddressValid(IPAddress ip) =>
+            !IPAddress.Loopback.Equals(ip);
 
         private void AddressChangedCallback(object sender, EventArgs e)
         {
             IPAddress myIP = GetMyIP();
-            if (!IPAddressValid(myIP) || localEndPoint.Address.Equals(myIP))
+            if (!IPAddressValid(myIP) || localEndpoint.Address.Equals(myIP))
             {
                 return;
             }
             logger.LogInformation("Address changed");
-            SetLocalEndPoint();
+            GetLocalEndPoint();
             if (shouldRun)
             {
                 Restart();
@@ -154,120 +98,41 @@ namespace ReceiverWinFormsApp.MessageReception
             Start();
         }
 
-        private void WaitForMessageListenerThread()
+        private async Task StartMessageReceptionist()
         {
-            if (messageListenerThread != null)
-            {
-                messageListenerThread.Join();
-            }
-        }
-
-        private void AcceptCallback(IAsyncResult ar)
-        {
-            // Signal thread to continue.
-            allDone.Set();
-            Socket listener = ar.AsyncState as Socket;
-            Socket handler;
+            messageListener = new(localEndpoint);
             try
             {
-                handler = listener.EndAccept(ar);
-            }
-            catch (ObjectDisposedException)
-            {
-                // We don't care why the socket was disposed.
-                return;
-            }
-            var state = new StateObject
-            {
-                workSocket = handler
-            };
-            handler.BeginReceive(
-                state.buffer,
-                0,
-                StateObject.BufferSize,
-                0,
-                new AsyncCallback(ReadCallback),
-                state);
-        }
+                messageListener.Start();
 
-        private void ReadCallback(IAsyncResult ar)
-        {
-            var message = string.Empty;
-            // Retrieve the state object and the handler socket
-            // from the asynchronous state object.
-            StateObject state = (StateObject)ar.AsyncState;
-            Socket handler = state.workSocket;
-            // Read data from the client socket.
-            int bytesRead = handler.EndReceive(ar);
-            if (bytesRead > 0)
-            {
-                // There  might be more data, so store the data received so far.
-                state.sb.Append(Encoding.ASCII.GetString(state.buffer, 0, bytesRead));
-                // Check for end-of-file tag. If it is not there, read
-                // more data.
-                if (state.sb[^1] == MessageTerminator)
+                while (shouldRun)
                 {
-                    // All the data has been read from the client.
-                    message = state.sb.ToString(0, state.sb.Length - 1);
-                    logger.LogInformation("Read {MessageLength} bytes from {IPAddress}. Data : {MessageContent}", message.Length, ((IPEndPoint)handler.RemoteEndPoint).Address, message);
-                    Maybe<string> maybeResponse = MessageProcessor(message);
-                    maybeResponse.Match(
-                        none: () => CloseHandler(handler),
-                        some: (response) => Send(handler, response));
+                    using TcpClient handler = await messageListener.AcceptTcpClientAsync();
+                    await using NetworkStream stream = handler.GetStream();
+                    using var memoryOwner = MemoryPool<byte>.Shared.Rent(128);
+                    var numberOfBytesRead = await stream.ReadAsync(memoryOwner.Memory);
+                    var message = Encoding.ASCII.GetString(memoryOwner.Memory[..numberOfBytesRead].Span);
+                    logger.LogInformation("Received message: {Message}", message);
+                    if (message[^1] != MessageTerminator)
+                    {
+                        logger.LogError("Received message does not end with the expected terminator character");
+                        return;
+                    }
+                    var response = MessageProcessor(message);
+                    response.Match(
+                        none: () => logger.LogInformation("No response generated for the message"),
+                        some: async responseMessage =>
+                        {
+                            var responseBytes = Encoding.ASCII.GetBytes(responseMessage);
+                            await stream.WriteAsync(responseBytes);
+                            logger.LogInformation("Sent response: {ResponseMessage}", responseMessage);
+                        });
                 }
-                else
-                {
-                    // Not all data received. Get more.
-                    handler.BeginReceive(
-                        state.buffer,
-                        0,
-                        StateObject.BufferSize,
-                        0,
-                        new AsyncCallback(ReadCallback),
-                        state);
-                }
-            }
-        }
-
-        private void Send(Socket handler, string data)
-        {
-            // Convert the string data to byte data using ASCII encoding.  
-            byte[] byteData = Encoding.ASCII.GetBytes(data + MessageTerminator);
-            logger.LogInformation("Sent \"{Data}\" to client", data);
-            // Begin sending the data to the remote device.  
-            handler.BeginSend(byteData, 0, byteData.Length, 0,
-                new AsyncCallback(SendCallback), handler);
-        }
-
-        private void SendCallback(IAsyncResult ar)
-        {
-            try
-            {
-                // Retrieve the socket from the state object.  
-                Socket handler = (Socket)ar.AsyncState;
-                // Complete sending the data to the remote device.  
-                int bytesSent = handler.EndSend(ar);
-                CloseHandler(handler);
-            }
-            catch (Exception ex)
-            {
-                logger.LogInformation(ex, "Send callback exception");
-            }
-        }
-
-        private void CloseHandler(Socket handler)
-        {
-            try
-            {
-                handler.Shutdown(SocketShutdown.Both);
-            }
-            catch (SocketException ex)
-            {
-                logger.LogInformation(ex, "Error closing handler");
             }
             finally
             {
-                handler.Close();
+                messageListener.Stop();
+                logger.LogInformation("Message receptionist is stopped");
             }
         }
 
